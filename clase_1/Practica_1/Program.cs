b99fe74cc8fa76f6964f385f0c4a16be34325180
Vianney Moreno <47407531+kaleix@users.noreply.github.com>
2020-01-22 12:59:03 -0700

    Hola_mundo

---
+++ clase_1/Practica_1/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Practica_1
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("Hola Mundo");
+            Console.GetKey();
+        }
+    }
+}
