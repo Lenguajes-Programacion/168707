f59f52270b3b342b452be8675e904d37374b9cda
Vianney Moreno <47407531+kaleix@users.noreply.github.com>
2020-01-24 12:59:17 -0700

    Programa del viernes

--- clase_1/Practica_1/Program.cs
+++ clase_1/Practica_1/Program.cs
@@ -4,9 +4,11 @@ namespace Practica_1
 {
     class Program
     {
-        static void Main(string args)
+        string nombre = "Vianney";
+
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola, " + this.nombre);
             Console.GetKey();
         }
     }
